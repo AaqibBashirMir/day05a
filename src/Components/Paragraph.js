b78f0e7066c53bc6c1ec70288c191a0ef4d9ed83
AaqibBashirMir <dazzlingmustakeen@gmail.com>
2023-12-25 21:36:59 +0530

    done

--- src/Components/Paragraph.js
+++ src/Components/Paragraph.js
@@ -5,45 +5,45 @@ import texts from './Data.js'
 
 function Paragraph() {
     const [text, setText] = useState([]);
-    const [count, setCount]=useState(0);
-    const submitHandler=(e)=>{
+    const [count, setCount] = useState(0);
+    const submitHandler = (e) => {
         e.preventDefault();
-        let tempTexts=parseInt(count);
-        if (tempTexts>8){
-               tempTexts=8;
-               alert(" Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀");
-            }
-            if(tempTexts<1){
-                alert("Please enter a number greater than zero");
-            }
-          setText(texts.slice(0,tempTexts))
+        let tempTexts = parseInt(count);
+        if (tempTexts > 8) {
+            tempTexts = 8;
+            alert(" Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀");
+        }
+        if (tempTexts < 1) {
+            alert("Please enter a number greater than zero");
+        }
+        setText(texts.slice(0, tempTexts))
 
     }
     return (
-        <div>
-            <h2>Paragrapgh Generator</h2>
-            <form onSubmit={submitHandler}>
-                <input
-                    type='number'
-                    id='input'
-                    value={count}
-                    onChange={(e)=>{setCount(e.target.value)}}
-                />
-                <button className='btn'>Generate</button>
-            </form>
-            <div style={{
-                margin:"100px",
-                border:"1px solid green"
-            }}>
-            {
-            text.map((list)=>{
-                return <p>{list}</p>
-            })
-           }
+        <div className='body'>
+            <div className='main'>
+                <h2>Paragrapgh Generator</h2>
+                <form onSubmit={submitHandler}>
+                    <input
+                        type='number'
+                        id='input'
+                        value={count}
+                        onChange={(e) => { setCount(e.target.value) }}
+                    />
+                    <button className='btn'>Generate</button>
+                </form>
+                <div id='text-field'>
+                    {
+                        text.map((list,idx) => {
+                            return <p key={idx} id='para'>{list}</p>
+                        })
+                    }
+                </div>
+
+
             </div>
-          
-           
-        </div> 
+        </div>
+
     )
 }
 
